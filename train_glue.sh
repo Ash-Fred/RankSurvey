c93de5f4b41369304d5267d72ba49cc6a4eda89a
Ash-Fred <102345870+Ash-Fred@users.noreply.github.com>
2024-03-05 08:41:40 +0900

    Add files via upload

--- train_glue.sh
+++ train_glue.sh
@@ -1,16 +1,49 @@
-# MNLI
-# Trained on a single V100 32GB
+#!/bin/bash
 
-MODEL_NAME=bert-base-uncased
-TASK_NAME=mnli #{cola,mnli,mrpc,qnli,qqp,rte,sst2,stsb,wnli}
-NO_EPOCHS=8
-PRUNE_RATIO=.1 #No of reduced params / No of original params
-DECOM_TYPE="fwsvd" #{svd,fwsvd,qr}
+while [[ $# -gt 0 ]]; do
+  key="$1"
+
+  case $key in
+  --task_name|-t)
+    TASK_NAME="$2"
+    shift # Shift past argument
+    ;;
+  --model_name_or_path|-m)
+    MODEL_NAME="$2"
+    shift
+    ;;
+  --num_train_epochs|-n)
+    NO_EPOCHS="$2"
+    shift
+    ;;
+  --low_rank_parameter_ratio|-r)
+    PRUNE_RATIO="$2"
+    shift
+    ;;
+  --decom_type|-d)
+    DECOM_TYPE="$2"
+    shift
+    ;;
+  --decom_type|-l)
+    LOSPARSE_ON="$2"
+    shift
+    ;;
+  --decom_type|-f)
+    FWSVDFILE="$2"
+    shift
+    ;;
+  *) # Unknown option
+    echo "Unknown option $key"
+    exit 1
+    ;;
+  esac
+  shift # Shift past argument or value
+done
 
 
 python3 run_glue.py \
---task_name $TASK_NAME \
---model_name_or_path $MODEL_NAME \
+--task_name ${TASK_NAME:-mnli} \
+--model_name_or_path ${MODEL_NAME:-bert-base-uncased} \
 --warmup_steps 0 \
 --initial_threshold 1 \
 --final_threshold 0.15 \
@@ -19,10 +52,11 @@ python3 run_glue.py \
 --beta1 0.85 \
 --per_device_train_batch_size 32 \
 --learning_rate 9e-5 \
---num_train_epochs $NO_EPOCHS \
+--num_train_epochs ${NO_EPOCHS:-'8'} \
 --output_dir output \
 --num_warmup_steps 480 \
---low_rank_parameter_ratio $PRUNE_RATIO \
+--low_rank_parameter_ratio ${PRUNE_RATIO:-'.1'} \
 --gradient_accumulation_steps 8 \
---decom_type $DECOM_TYPE \
---fwsvdfile
+--decom_type ${DECOM_TYPE:-fwsvd} \
+--apply_LoSparse ${LOSPARSE_ON:-'1'} \
+--fwsvdfile 0
